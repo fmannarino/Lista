97333cacb31a92bbb88b1a6babf09ab0930f3ee0
felipemannarino <mannarino1@hotmail.com>
2018-05-02 17:58:45 -0300

    Login via instagram

---
+++ Lista/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Lista
+//
+//  Created by Felipe Mannarino on 30/04/2018.
+//  Copyright © 2018 Felipe Mannarino. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let AUTHURL = "https://api.instagram.com/oauth/authorize/"
+    static let APIURL = "https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&"
+    static let CLIENTID = "ec4898fd109f47bcb301164090f34f37"
+    static let CLIENTESECRET = "99e9e4cfd78a46fba992874d66276ac5"
+    static let REDIRECTURL = "http://www.lista.com.br"
+    static let ACCESSTOKEN = "access_token"
+    static let SCOPE = "likes+comments+relationships"
+}
---
+++ Lista/InstagramViewController.swift
@@ -0,0 +1,77 @@
+//
+//  InstagramViewController.swift
+//  Lista
+//
+//  Created by Felipe Mannarino on 30/04/2018.
+//  Copyright © 2018 Felipe Mannarino. All rights reserved.
+//
+
+import UIKit
+
+class InstagramViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.delegate = self
+        loginRequest()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loginRequest() {
+        let url = "\(Constants.AUTHURL)?client_id=\(Constants.CLIENTID)&redirect_uri=\(Constants.REDIRECTURL)&response_type=token&scope=\(Constants.SCOPE)&DEBUG=True"
+        let request = URLRequest.init(url: URL.init(string: url)!)
+        webView.loadRequest(request)
+    }
+    func checkRequestForCallBack(request: URLRequest) -> Bool {
+        let UrlString = (request.url?.absoluteString)! as String
+        
+        if UrlString.hasPrefix(Constants.REDIRECTURL) {
+            let range: Range<String.Index> = UrlString.range(of: "#access_token=")!
+            getAccessToken(authToken: UrlString.substring(from: range.upperBound))
+            return false
+        }
+        return true
+    }
+    
+    func getAccessToken(authToken: String) {
+        self.navigationController?.popViewController(animated: true)
+        let url = "\(Constants.APIURL)access_token=\(authToken)"
+        var request: URLRequest = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "GET"
+        request.cachePolicy = URLRequest.CachePolicy.reloadIgnoringCacheData
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        session.dataTask(with: request) { (data, response, error) -> Void in
+            if let data = data {
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
+                print(json)
+                let strFullName = (json?.value(forKey: "data") as AnyObject).value(forKey: "filter") as? String
+                
+                let alert = UIAlertController(title: "FULL NAME", message: strFullName, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: NSLocalizedString("OK", comment: "Ok action"), style: .default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true)
+            }
+        }.resume()
+    }
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        return checkRequestForCallBack(request: request)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
